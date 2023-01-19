a4908087eab10b9b0c89d13d859019a05b842a44
Leidys Matos <matosleidys@gmail.com>
2023-01-19 16:51:54 -0400

    guardado

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -15,6 +15,12 @@ const BlogPost = () => {
   const blogpost = blogData.find((post) => post.slug === slug);
   const canDelete =
     auth.user?.isAdmin || blogpost.author === auth.user?.username;
+
+  const deleteBlog = (titulo) => {
+    const deleteSelect = blogData.filter((title) => title.title !== titulo);
+    console.log(deleteSelect);
+    return deleteSelect;
+  };
   return (
     <div className="ms-4 mt-4">
       <button className="btn btn-primary my-2" onClick={returnalblog}>
@@ -24,7 +30,14 @@ const BlogPost = () => {
       <p className="fs-2">{blogpost.content}</p>
       <p className="fs-4">{blogpost.author}</p>
 
-      {canDelete && <button className="btn btn-danger mt-2">Eliminar</button>}
+      {canDelete && (
+        <button
+          className="btn btn-danger mt-2"
+          onClick={() => deleteBlog(blogpost.title)}
+        >
+          Eliminar
+        </button>
+      )}
     </div>
   );
 };
